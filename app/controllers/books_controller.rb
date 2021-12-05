8d5dc9f26f89692d8e07beaa43010614465cdd84
yuuuba <nsabu45@gmail.com>
2021-12-05 15:55:01 +0900

    コントローラーの修正

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,37 +1,4 @@
 class BooksController < ApplicationController
   def index
-    @books = Book.all 
   end
-
-  def show
-    @detail = Book.find_by(params[:id])
-  end
-
-  def new
-    @book = Book.new #空のBookモデルクラスのインスタンス作成
-  end
-
-  def create
-    @books = Book.create(book_params)
-    redirect_to books_path
-  end
-
-  def edit
-    @detail = Book.find_by(params[:id])
-  end
-
-  def update
-    @detail = Book.find_by(params[:id])
-  end
-
-  def destroy
-    @detail = Book.find_by(params[:id])
-  end
-
-  private
-
-  def book_params
-    params.require(:book).permit(:title, :score)#改竄されて許可していない内容送られるのを防ぐためprivate
-  end
-
 end
