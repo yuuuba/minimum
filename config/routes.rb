b6be59448db394b7692cca6b563b5b6647801f06
yuuuba <nsabu45@gmail.com>
2021-12-05 15:54:33 +0900

    ルーティングの変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,3 @@
 Rails.application.routes.draw do
-  #get '/bookshelf' , to: 'books#index' , as: 'bookshelf'
-  #post '/bookshelf' , to: 'books#create' 
-  #get '/bookshelf/new', to: 'books#new'
-  #get '/bookshelf/:id', to: 'books#show', as: 'bookshelf_id'
-  resources :books
+  resources :books, only: [:index]
 end
