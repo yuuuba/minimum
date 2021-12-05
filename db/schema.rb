1881f11644806a45f0c08ddbdebb34782d6a90f5
yuuuba <nsabu45@gmail.com>
2021-12-05 15:52:34 +0900

    migrate実行

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_02_151428) do
+ActiveRecord::Schema.define(version: 2021_12_05_064135) do
 
   create_table "books", charset: "utf8mb4", force: :cascade do |t|
-    t.text "title"
-    t.integer "score"
+    t.string "name"
+    t.integer "price"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
